20c011b86961de4f00fd0533e5914e3e9d995164
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-02-04 17:37:14 -0500

    updated token and env

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const auth = require('./auth.json');
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -27,4 +26,4 @@ client.on('message', msg => {
 	
 });
 
-client.login(auth.token);
+client.login(process.env.DISCORD_TOKEN);
