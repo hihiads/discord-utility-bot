e2d7bdb67d4b1b3a58d6fb09a1fce71cca5e68ad
John James Yzaguirre <johnyzaguirre@vassar.edu>
2020-02-12 17:40:08 -0500

    review bot refactor completed

--- index.js
+++ index.js
@@ -1,228 +1,73 @@
-// download the gCreds.json for google api scripts
-// file system module to perform file operations
+let { reviewBot } = require( './bots/reviewBot.js' )
+
+// create google api creds file
 const fs = require('fs');
- 
-// json data
-var jsonData = process.env.gJSON
- 
-// parse json
-var jsonObj = JSON.parse(jsonData);
-console.log('gcreds obj successfully created');
- 
-// stringify JSON Object
-var jsonContent = JSON.stringify(jsonObj);
-console.log('gcreds content successfully created');
- 
-fs.writeFile("gCreds.json", jsonContent, 'utf8', function (err) {
-    if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
-    }
- 
-    console.log("JSON file has been saved.");
-});
 
-// use nodemon index.js for local development for auto file reload
-const {
-  getAuthToken,
-  postSpreadSheetValues,
-  getSpreadSheet
-} = require('./googleSheetsService.js');
-
-const spreadsheetId = '1uCkgD4sSi5-SBtuMQkIxa_NFCp36SJLO7b_97zf0M9A';
-const sheetName = 'reviews';
-
-const Discord = require('discord.js')
-const client = new Discord.Client()
-
-let content, user, username, botName, messageObj, sentMessage, dmChannel, response
-
-let coach, student
-
-// all bot messages
-let score = 'Hello! Please rate your lesson ' 
-score += 'from 1 - 10 (10 being awesome)'
-
-let comment = 'Thanks! Lastly, please leave a short comment '
-comment += 'telling us what you thought about the lesson'
-
-let thanks = 'Thanks for your input!'
-
-let notPermitted = 'sorry only coaches can summon me to do things!'
-
-const botMsgs = {score: score, comment: comment, thanks: thanks, notPermitted: notPermitted}
-
-
-const getMessage = (msgObj) => new Promise((resolve, reject) => {
-	const content = msgObj.content.split( " " )
-	if (content[0] !== '!review') return
-	if ( !hasCoachID(msgObj) ){
-		msgObj.reply( botMsgs.notPermitted )
-		return
-	}
-	resolve(msgObj)
-})
-
-
-
-
-const sendMessage = (studentObj, botMsg) => new Promise((resolve, reject) => {
-	botName = client.user.username
-
-	if (studentObj.username === botName){
-		console.log('not responding to my own message!')
-		return
-	}
-
-	resolve(studentObj.send(botMsg))
-})
-
-
-
-
-const collectScore = (sentMsgObj) => new Promise((resolve, reject) => {
-	const filter = m => (m.content > 0 && m.content < 11)
-	dmChannel = sentMsgObj.channel
-
-	resolve(dmChannel.awaitMessages(filter, { max: 1 }))
-})
-
-
-const collectComment = (sentMsgObj) => new Promise((resolve, reject) => {
-	const filter = m => (typeof m.content === 'string')
-	dmChannel = sentMsgObj.channel
-
-	resolve(dmChannel.awaitMessages(filter, { max: 1 }))
-})
-
-
-const main = async (msgObj) => {
-
-	// start dm review in channel with !review
-	let messageObj = await getMessage(msgObj)
-
-	let content = messageObj.content.split(" ")
-	let command = content[1]
-	
-	if ( command === 'me' )
-	{	
-
-		// get name of coach
-		coach = messageObj.author.username
-
-		// get student user object
-		let rawID = content[2]
-		let studentID = rawID.substring(3, rawID.length-1)
-		let studentObj = await client.fetchUser(studentID)
-
-		// student = studentObj.lastMessage.member.nickname
-		student = studentObj.username
-
-		// ask for score & collect response
-		let sentMessage = await sendMessage(studentObj, botMsgs.score)
-		let scoreCollectedObj = await collectScore(sentMessage)
-		let score = scoreCollectedObj.first().content
-
-		// ask for comment & collect response
-		sentMessage = await sendMessage(studentObj, botMsgs.comment)
-		let commentCollectedObj = await collectComment(sentMessage)
-		let comment = await commentCollectedObj.first().content
-
-		// complete the conversation
-		sentMessage = await sendMessage(studentObj, botMsgs.thanks)
-		
-
-		// post to the google sheet
-		let values = [coach, student, score, comment]
-		const auth = await getAuthToken();
-		response = await postSpreadSheetValues({spreadsheetId, auth, sheetName, values})
-
-		return response
-	}
-
-	if ( command === 'stats' )
-	{  
-		// get coach nickname
-		let rawID = content[2]
-		let coachID = rawID.substring(3, rawID.length-1)
-		let coachObj = await client.fetchUser(coachID)
-
-		coach = coachObj.username
-
-		const auth = await getAuthToken();
-		const data = await getSpreadSheet({spreadsheetId, auth, sheetName})
-
-		let coachData = getCoachRows(data, coach)
-
-		let avgRating = getAvgRating(coachData)
-
-		response = await messageObj.author.send( 'Average review for ' + coach + ' is ' + avgRating.toFixed(2) )
-
-
-		return response
-	}
+if (!fs.existsSync('./gCreds.json')) {
+    
+	// json data
+	var jsonData = process.env.gJSON
+	 
+	// parse json
+	var jsonObj = JSON.parse(jsonData);
+	console.log('gcreds obj successfully created');
+	 
+	// stringify JSON Object
+	var jsonContent = JSON.stringify(jsonObj);
+	console.log('gcreds content successfully created');
+	 
+	fs.writeFile("./gCreds.json", jsonContent, 'utf8', function (err) {
+	    if (err) {
+	        console.log("An error occured while writing JSON Object to File.");
+	        return console.log(err);
+	    }
+	 
+	    console.log("JSON file has been saved.");
+	});
 
 }
 
+// connect to discord
+const Discord = require('discord.js')
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+global.client = new Discord.Client()
+client.login(DISCORD_TOKEN)
+client.on( 'ready', msg => console.log( 'bot connected' ))
 
-client.login(process.env.DISCORD_TOKEN)
 
-
-
-
-client.on( 'ready', msg => console.log('bot connected'))
+// run main() when new messages detected on the server
 client.on( 'message', msg => {
-	main(msg)
+	// check if we're being summoned and return the correct bot
+	// split up message into an array
+	const message =  msg.content.split( " " )
+
+	// ignore messages that do not start with !
+	if (message[0][0] !== '!')
+		return
+
+	if(message[1] === undefined)
+		reject( 'command rejected in summonBot.js' )
+
+	// resolve with an object - bot's name, bot's command and original message as an array
+	let request = { 
+		'name': message[0].slice(1,message[0].length), 
+		'command': message[1],
+		'message': message,
+		'msgObj': msg
+	}
+
+	const controller = new Map( [['review', reviewBot]] )
+
+	let bot = controller.get(request.name)
+
+	bot(request)
 	.then( (response) => { 
-		console.log(response);
+		console.log(response)
 	} )
 	.catch( ( error ) => { 
-		console.log( 'WHOOPS ERROR: ' + error )
+		console.log( 'whoops: ' + error )
 	} )
 	.finally( ( data ) => { 
-		console.log( 'bot finally out!' )
+		console.log( 'bot finally out!')
 	} )
-
 })
-
-
-
-// HELPERS
-function hasCoachID(messageObj) {
-	let roles = Array.from(messageObj.guild.roles)
-	
-	let guildRoles = [];
-
-	for (var i = 0; i < roles.length; i++) {
-		temp = {};
-		temp.id = roles[i][0]
-		temp.name = roles[i][1].name
-		guildRoles.push(temp)
-	}
-
-	let hasRole = false
-
-	coachID = guildRoles.filter( role =>  role.name === 'coach')[0].id
-
-	if(messageObj.member.roles.has(coachID)) {
-	  hasRole = true
-	}
-
-	return hasRole
-}
-
-
-
-function getCoachRows(data, coach) {
-	data = Array.from(data)
-	return(data.filter( data => data[0] === coach ))
-}
-
-function getAvgRating(coachData) {
-	let scores = [];
-	for (var i = 0; i < coachData.length; i++) {
-		let score = coachData[i][2]
-		scores.push( parseInt(score) )
-	}
-	return scores.reduce( (total, sum) => total + sum) / scores.length
-}
