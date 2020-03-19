8b7ab2535df9b60aadadfdc7fc76ce7fba0bd4cb
Vladimir Isakov <callback.me@protonmail.com>
2020-03-19 17:42:22 +0300

    Add support for custom bot commands prefix

--- bots/helpBot.js
+++ bots/helpBot.js
@@ -3,7 +3,7 @@ helpBot = async (message) => {
 	const helpMessage = {
 		color: 0x0099ff,
 		title: 'DotaFromZero bot',
-		description: "\nCommands:\n\n!dfz lobby <date> <time> <timezone> <lobbytype>\n\nexample: !dfz lobby 3/15/20 9:00pm EST meme\n\nlobby types: meme, open or just leave blank\nto ping tiers 1 & 2",
+		description: `\nCommands:\n\n${PREFIX} lobby <date> <time> <timezone> <lobbytype>\n\nexample: ${PREFIX} lobby 3/15/20 9:00pm EST meme\n\nlobby types: meme, open or just leave blank\nto ping tiers 1 & 2`,
 		thumbnail: {
 			url: 'http://getdrawings.com/free-icon/robot-icon-png-57.png',
 		},
